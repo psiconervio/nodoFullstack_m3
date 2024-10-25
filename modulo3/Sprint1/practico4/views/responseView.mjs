ecc4a3fae2c4159c35cca30632b2c246257f2b0e
psiconervio <psiconervio@gmail.com>
2024-10-25 12:35:29 -0300

    asdasd

--- modulo3/Sprint1/practico4/views/responseView.mjs
+++
@@ -1,9 +0,0 @@
-// views/tareaView.mjs
-//funcion para renderizar una lista de tareas en formato JSON
-export function renderizarListaTareas(tareas) {
-    return JSON.stringify(tareas, null, 2);
-  }
-//funcion para renderizar un mensaje generico en formato json
-  export function renderizarTarea(tarea) {
-    return JSON.stringify(tarea, null, 2);
-  }
