7de14bdf394be6c7c2f5e2c9f8c806c1c26e96d4
psiconervio <psiconervio@gmail.com>
2024-10-30 21:25:24 -0300

    skere

--- modulo3/Sprint2/tpn1/app.js
+++ modulo3/Sprint2/tpn1/app.js
@@ -1,92 +1,92 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://usuario:contraseña@cluster0.mongodb.net/PracticaSuperHeroes', {
+mongoose.connect('mongodb+srv://Grupo-18:grupo18@cursadanodejs.ls9ii.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Conexión exitosa a MongoDB'))
 .catch(error => console.error('Error al conectar a MongoDB:', error));
 
-//esquema
-const superheroSchema = new mongoose.Schema({
-    nombreSuperHeroe: { type: String, required: true },
-    nombreReal: { type: String, required: true },
-    edad: { type: Number, min: 0 },
-    planetaOrigen: { type: String, default: 'Desconocido' },
-    debilidad: String,
-    poderes: [String],
-    aliados: [String],
-    enemigos: [String],
-    createdAt: { type: Date, default: Date.now }
-  });
+// //esquema
+// const superheroSchema = new mongoose.Schema({
+//     nombreSuperHeroe: { type: String, required: true },
+//     nombreReal: { type: String, required: true },
+//     edad: { type: Number, min: 0 },
+//     planetaOrigen: { type: String, default: 'Desconocido' },
+//     debilidad: String,
+//     poderes: [String],
+//     aliados: [String],
+//     enemigos: [String],
+//     createdAt: { type: Date, default: Date.now }
+//   });
   
-  const SuperHero = mongoose.model('SuperHero', superheroSchema);
+//   const SuperHero = mongoose.model('SuperHero', superheroSchema);
 
 
 
-  //funciones 
-async function insertSuperHero() {
-    const hero = new SuperHero({
-      nombreSuperHeroe: 'Spiderman',
-      nombreReal: 'Peter Parker',
-      edad: 25,
-      planetaOrigen: 'Tierra',
-      debilidad: 'Radioactiva',
-      poderes: ['Trepar paredes', 'Sentido arácnido', 'Super fuerza', 'Agilidad'],
-      aliados: ['Ironman'],
-      enemigos: ['Duende Verde']
-    });
+//   //funciones 
+// async function insertSuperHero() {
+//     const hero = new SuperHero({
+//       nombreSuperHeroe: 'Spiderman',
+//       nombreReal: 'Peter Parker',
+//       edad: 25,
+//       planetaOrigen: 'Tierra',
+//       debilidad: 'Radioactiva',
+//       poderes: ['Trepar paredes', 'Sentido arácnido', 'Super fuerza', 'Agilidad'],
+//       aliados: ['Ironman'],
+//       enemigos: ['Duende Verde']
+//     });
   
-    await hero.save();
-    console.log('Superhéroe insertado:', hero);
-  }
+//     await hero.save();
+//     console.log('Superhéroe insertado:', hero);
+//   }
   
-  insertSuperHero();
+//   insertSuperHero();
 
 
 
 
-  async function insertSuperHero() {
-    const hero = new SuperHero({
-      nombreSuperHeroe: 'Spiderman',
-      nombreReal: 'Peter Parker',
-      edad: 25,
-      planetaOrigen: 'Tierra',
-      debilidad: 'Radioactiva',
-      poderes: ['Trepar paredes', 'Sentido arácnido', 'Super fuerza', 'Agilidad'],
-      aliados: ['Ironman'],
-      enemigos: ['Duende Verde']
-    });
+//   async function insertSuperHero() {
+//     const hero = new SuperHero({
+//       nombreSuperHeroe: 'Spiderman',
+//       nombreReal: 'Peter Parker',
+//       edad: 25,
+//       planetaOrigen: 'Tierra',
+//       debilidad: 'Radioactiva',
+//       poderes: ['Trepar paredes', 'Sentido arácnido', 'Super fuerza', 'Agilidad'],
+//       aliados: ['Ironman'],
+//       enemigos: ['Duende Verde']
+//     });
   
-    await hero.save();
-    console.log('Superhéroe insertado:', hero);
-  }
+//     await hero.save();
+//     console.log('Superhéroe insertado:', hero);
+//   }
   
-  insertSuperHero();
+//   insertSuperHero();
 
 
-  async function updateSuperHero(nombreSuperHeroe) {
-    const result = await SuperHero.updateOne(
-      { nombreSuperHeroe: nombreSuperHeroe },
-      { $set: { edad: 26 } }
-    );
-    console.log('Resultado de la actualización:', result);
-  }
+//   async function updateSuperHero(nombreSuperHeroe) {
+//     const result = await SuperHero.updateOne(
+//       { nombreSuperHeroe: nombreSuperHeroe },
+//       { $set: { edad: 26 } }
+//     );
+//     console.log('Resultado de la actualización:', result);
+//   }
   
-  updateSuperHero('Spiderman');
+//   updateSuperHero('Spiderman');
 
 
-  async function deleteSuperHero(nombreSuperHeroe) {
-    const result = await SuperHero.deleteOne({ nombreSuperHeroe: nombreSuperHeroe });
-    console.log('Superhéroe eliminado:', result);
-  }
+//   async function deleteSuperHero(nombreSuperHeroe) {
+//     const result = await SuperHero.deleteOne({ nombreSuperHeroe: nombreSuperHeroe });
+//     console.log('Superhéroe eliminado:', result);
+//   }
   
-  deleteSuperHero('Spiderman');
+//   deleteSuperHero('Spiderman');
 
 
-  async function findSuperHeroes() {
-    const heroes = await SuperHero.find({ planetaOrigen: 'Tierra' });
-    console.log('Superhéroes encontrados:', heroes);
-  }
+//   async function findSuperHeroes() {
+//     const heroes = await SuperHero.find({ planetaOrigen: 'Tierra' });
+//     console.log('Superhéroes encontrados:', heroes);
+//   }
   
-  findSuperHeroes();
+//   findSuperHeroes();
