49e7980692f51b05f86f08f63894d5fed903eba7
psiconervio <psiconervio@gmail.com>
2025-01-22 18:05:16 -0300

    SDASD

--- modulo3/Sprint5/tpn1/controllers/countryController.mjs
+++ modulo3/Sprint5/tpn1/controllers/countryController.mjs
@@ -6,16 +6,24 @@ import {
   eliminarPais,
 } from '../services/CountryService.mjs';
 //Actúan como intermediarios entre las rutas y los servicios/modelos.
-//Los controladores reciben las solicitudes, llaman a los servicios o modelos para realizar tareas específicas, y luego devuelven una respuesta al cliente.
+//Los controladores reciben las solicitudes, llaman a los servicios o modelos para realizar tareas específicas,
+//  y luego devuelven una respuesta al cliente.
+
 export async function obtenerTodosLosPaisesController(req, res) {
+  // llama a la funcion obtenerTodosLosPaises() del servicio CountryService
   const paises = await obtenerTodosLosPaises();
+  // responde con un json con los paises obtenidos
   res.json(paises);
 }
 
 export async function obtenerPaisPorIdController(req, res, next) {
+  // obtiene el id del pais de los parametros de la solicitud
   const { id } = req.params;
+  // llama a la funcion obtenerPaisPorId() del servicio CountryService
   const pais = await obtenerPaisPorId(id);
+  // si no se encuentra el pais, responde con un error
   if (!pais) return res.status(404).json({ error: 'País no encontrado' });
+  // si se encuentra el pais, lo agrega a la solicitud y llama al siguiente middleware
   req.country = pais;
   // res.json(pais);
   next();
--- modulo3/Sprint5/tpn1/services/CountryService.mjs
+++ modulo3/Sprint5/tpn1/services/CountryService.mjs
@@ -2,6 +2,7 @@ import countryRepository from '../repositories/CountryRepository.mjs';
 //Los servicios procesan datos, realizan validaciones avanzadas, o coordinan llamadas a los repositories.
 //el servicio ejecuta desde la clase countryRepository la funcion obtenerTodos()
 export async function obtenerTodosLosPaises() {
+  // de la clase countryRepository ejecuta la funcion obtenerTodos()
   return await countryRepository.obtenerTodos();
 }
 
